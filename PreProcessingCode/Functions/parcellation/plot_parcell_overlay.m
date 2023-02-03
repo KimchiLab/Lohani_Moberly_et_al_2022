34aec25461c809b7449dee70d1427ba7d4db04f8
Sweyta Lohani <38272619+lohan20s@users.noreply.github.com>
2023-02-03 16:50:47 -0500

    Update plot_parcell_overlay.m

--- PreProcessingCode/Functions/parcellation/plot_parcell_overlay.m
+++ PreProcessingCode/Functions/parcellation/plot_parcell_overlay.m
@@ -7,8 +7,8 @@ if plotSave
     for k=1:size(parcells,3)
         E=parcells(:,:,k);
         [B,~] = bwboundaries(E);hold on;
-        for k = 1:length(B)
-            boundary = B{k};
+        for t = 1:length(B)
+            boundary = B{t};
             plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 0.1)
         end
     end
